c7333ec7d3335a6f53a54e01c95a0121bb55d6ba
YangNianYi <y790174683@163.com>
2018-08-27 15:04:13 +0800

    oo0

--- 03.urllib3.py
+++ 03.urllib3.py
@@ -2,7 +2,7 @@ import urllib3
 
 http = urllib3.PoolManager()
 
-response = http.request('GET', 'http://www.zhihu.com')
+response = http.request('GET', 'http://www.baidu.com')
 
 print(response.status)
 print(response.data.decode())
