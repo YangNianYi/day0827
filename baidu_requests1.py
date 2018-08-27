b1cbd8d270e936ee3db3e68eb4cfd594641cc9e9
YangNianYi <y790174683@163.com>
2018-08-27 15:34:08 +0800

    oo0

--- baidu_requests1.py
+++ baidu_requests1.py
@@ -1,7 +1,12 @@
 import requests
 
-response = requests.get('https://www.baidu.com')
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+}
 
+response = requests.get('https://www.baidu.com', headers=headers)
+
+print(response.headers)
 with open(r'baidu.html', 'wb') as f:
     f.write(response.content)
 
